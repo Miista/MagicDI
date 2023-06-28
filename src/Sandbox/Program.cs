33971d825d18495b70211da8c8440cb582e344ad
Søren Palmund <palmund@gmail.com>
2023-06-28 18:09:25 +0200

    Add basic implementation

--- src/Sandbox/Program.cs
+++ src/Sandbox/Program.cs
@@ -9,4 +9,21 @@ namespace Sandbox
             Console.WriteLine("Hello World!");
         }
     }
+
+    class MagicDI
+    {
+        public T Resolve<T>()
+        {
+            if (typeof(T).IsPrimitive)
+                throw new InvalidOperationException(
+                    $"Cannot resolve instance of type {typeof(T).Name} because it is a primitive type");
+            
+            // 1. Find the most appropriate constructor
+            // 2. Resolve arguments to said constructor
+            // 3. Invoke constructor
+            // 4. Return created object
+
+            return default(T);
+        }
+    }
 }
