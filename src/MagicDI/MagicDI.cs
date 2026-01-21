50c0fefbaf451f8815c8df6cae31e520976ca67c
Søren Guldmund <palmund@gmail.com>
2026-01-21 21:53:04 +0100

    Extract ResolveInstance to InstanceFactory class

--- src/MagicDI/MagicDI.cs
+++ src/MagicDI/MagicDI.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
 
 namespace MagicDI
 {
@@ -21,10 +18,17 @@ namespace MagicDI
         private readonly LifetimeResolver _lifetimeResolver = new();
 
         /// <summary>
-        /// Tracks types currently being resolved on each thread to detect circular dependencies.
-        /// Using ThreadLocal ensures thread-safety for the resolution stack.
+        /// Creates instances of types by resolving constructor dependencies.
         /// </summary>
-        private readonly ThreadLocal<HashSet<Type>> _resolutionStack = new(() => []);
+        private readonly InstanceFactory _instanceFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MagicDI"/> class.
+        /// </summary>
+        public MagicDI()
+        {
+            _instanceFactory = new InstanceFactory(Resolve);
+        }
 
         /// <summary>
         /// Resolves an instance of the specified type, automatically
@@ -70,84 +74,14 @@ namespace MagicDI
                         return cached;
                     }
 
-                    var instance = ResolveInstance(type);
+                    var instance = _instanceFactory.CreateInstance(type);
                     _singletons.Add(type, instance);
                     return instance;
                 }
             }
 
             // Transient - no locking needed, create new instance each time
-            return ResolveInstance(type);
-        }
-
-        /// <summary>
-        /// Creates a new instance of the specified type by finding and invoking the appropriate constructor.
-        /// </summary>
-        /// <param name="type">The type to instantiate.</param>
-        /// <returns>A new instance of the specified type.</returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when the type is a primitive type, when a circular dependency is detected,
-        /// or when constructor invocation returns null.
-        /// </exception>
-        private object ResolveInstance(Type type)
-        {
-            if (type.IsPrimitive)
-                throw new InvalidOperationException(
-                    $"Cannot resolve instance of type {type.Name} because it is a primitive type");
-
-            // Check for circular dependency
-            if (!_resolutionStack.Value.Add(type))
-            {
-                var chain = string.Join(" -> ", _resolutionStack.Value.Select(t => t.Name)) + " -> " + type.Name;
-                throw new InvalidOperationException(
-                    $"Circular dependency detected while resolving {type.Name}. Resolution chain: {chain}");
-            }
-
-            try
-            {
-                // 1. Find the most appropriate constructor
-                var constructorInfo = GetConstructor(type);
-
-                // 2. Resolve arguments to said constructor
-                var resolvedConstructorArguments = ResolveConstructorArguments(constructorInfo);
-
-                // 3. Invoke constructor
-                var instance = constructorInfo.Invoke(resolvedConstructorArguments);
-
-                if (instance == null)
-                    throw new InvalidOperationException(
-                        $"Constructor invocation for type {type.Name} returned null");
-
-                return instance;
-            }
-            finally
-            {
-                // Always remove the type from the resolution stack when done
-                _resolutionStack.Value.Remove(type);
-            }
-        }
-
-        /// <summary>
-        /// Gets the most appropriate constructor for the specified type.
-        /// Selects the constructor with the most parameters, using metadata token order as a tiebreaker.
-        /// </summary>
-        /// <param name="type">The type to get the constructor for.</param>
-        /// <returns>The selected constructor.</returns>
-        /// <exception cref="InvalidOperationException">Thrown when the type has no public constructors.</exception>
-        private static ConstructorInfo GetConstructor(Type type) => LifetimeResolver.GetConstructor(type);
-
-        /// <summary>
-        /// Resolves all constructor parameters by recursively resolving each parameter type.
-        /// </summary>
-        /// <param name="constructorInfo">The constructor whose parameters should be resolved.</param>
-        /// <returns>An array of resolved parameter instances.</returns>
-        private object[] ResolveConstructorArguments(ConstructorInfo constructorInfo)
-        {
-            return constructorInfo
-                .GetParameters()
-                .Select(info => info.ParameterType)
-                .Select(Resolve)
-                .ToArray();
+            return _instanceFactory.CreateInstance(type);
         }
     }
 }
